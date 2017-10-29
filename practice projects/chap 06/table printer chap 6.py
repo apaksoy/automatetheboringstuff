6a23e968affbfd4c80729b7029a495181ea208c1
alperpaksoy <33169721+alperpaksoy@users.noreply.github.com>
2017-10-29 23:43:55 +0300

    Update table printer chap 6.py

--- practice projects/chap 06/table printer chap 6.py
+++ practice projects/chap 06/table printer chap 6.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 '''
-Write a function named printTable() that takes a list of lists of strings and displays it in a well-organized table with each column right-justified. Assume that all the inner lists will contain the same number of strings. For example, the value could look like this:
+Write a function named printTable() that takes a list of lists
+of strings and displays it in a well-organized table with each
+column right-justified. Assume that all the inner lists will
+contain the same number of strings. For example, the value
+could look like this:
 
 
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
