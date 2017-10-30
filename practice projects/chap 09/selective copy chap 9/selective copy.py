822bb3a1c6461a185d18937d635712fce5ee3c15
apaksoy <33169721+apaksoy@users.noreply.github.com>
2017-10-30 13:57:36 +0300

    Create selective copy.py

---
+++ practice projects/chap 09/selective copy chap 9/selective copy.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+#  This program copies all specified types of files under a specified
+#  directory to a new folder
+'''
+Write a program that walks through a folder tree and searches for files
+with a certain file extension (such as .pdf or .jpg). Copy these files
+from whatever location they are in to a new folder.
+'''
+
+import os
+import re
+import shutil
+import sys
+
+fileTypes = ('jpg', 'pdf')
+
+# Confirm the existence and get the absolute path of the source
+# and target folders
+
+sourceFolder = './source'
+sourceFolder = os.path.abspath(sourceFolder)
+simpleSourceFolder = re.search(r'/([\w .-]+)$', sourceFolder).group(1)
+
+targetFolder = './target'
+targetFolder = os.path.abspath(targetFolder)
+simpleTargetFolder = re.search(r'/([\w .-]+)$',targetFolder).group(1)
+
+if os.path.exists(sourceFolder):
+    print('Will copy file types:\n')
+    for fileType in fileTypes:
+        print('%s' % (fileType))
+    print('\nin "%s" folder \n' % (simpleSourceFolder))
+else:
+    print('Source folder %s does not exist.' % (sourceFolder))
+    sys.exit()
+
+if os.path.exists(targetFolder):
+    print('\nto target folder "%s" \n' % (simpleTargetFolder))
+else:
+    print('Target folder %s does not exist.' % (targetFolder))
+    sys.exit()
+
+# Loop through the directory and its subdirectories
+# to locate the specified types of files
+# and paste them to the target directory
+count = 0
+for foldername, subfolders, filenames in os.walk(sourceFolder):
+    print('\nCopying specified file types in "%s"...' % (os.path.basename(foldername)))
+    for filename in filenames:
+        for fileType in fileTypes:
+            if filename.endswith('.' + fileType):
+                # print('%s\nto\n%s' % (os.path.join(foldername, filename) , targetFolder)) # dry run
+                print(shutil.copy(os.path.join(foldername, filename), targetFolder))
+                count += 1
+
+print('\nDone. %s files copied.' %(count))
