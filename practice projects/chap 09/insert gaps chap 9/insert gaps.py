974569e74c52e813fa8901a056f034a66bbb87d2
apaksoy <33169721+apaksoy@users.noreply.github.com>
2017-11-01 00:13:52 +0300

    Update insert gaps.py

--- practice projects/chap 09/insert gaps chap 9/insert gaps.py
+++ practice projects/chap 09/insert gaps chap 9/insert gaps.py
@@ -18,10 +18,13 @@ import sys
 
 
 def insertGap(stList):
-    ''' Finds the smallest gap in integer values of a series of numbers in
-        strings and increases the values of all strings by 1 smaller than
-        the gap. All values are raised by 1 if there are no gaps. The series
-        is assumed to be already sorted in increasing order '''
+'''
+Finds the smallest gap in integer values of a series of numbers in
+strings and increases the values of all strings by 1 smaller than
+the gap. All values are raised by 1 if there are no gaps. The series
+is assumed to be already sorted in increasing order as it is received.
+See the flowchart to understand more easily how this function works. 
+'''
     
     # convert string list to integer list
     i = 0
@@ -61,104 +64,97 @@ def insertGap(stList):
     return stList
 
 
+# Confirm existence of the specified folder
+sourceFolder = './source'
+
+sourceFolder = os.path.abspath(sourceFolder)
+# simpleSourceFolder = re.search(r'/([\w .-]+)$',sourceFolder).group(1)
+
+startStr = 'spam'
+insList = ['001', '003'] # serial numbers to be inserted
+print('insList: ', insList)
+
+# Confirm existence of specified directory
+if os.path.exists(sourceFolder):
+    # Print a "welcome" message
+    print('\nWill insert gaps in numbering to text files'\
+          ' starting with "' + startStr + '" if necessary:')
+    print(sourceFolder, '\n')
+else:
+    print('Folder %s does not exist.' % (sourceFolder))
+    sys.exit()
+
+# Get the names of the files with the specified prefix
+fileList = os.listdir(sourceFolder)
+
+pattern = r'spam(\d*)\.txt'
+orSerList = [] # original file serial numbers
+
+allFileCount = 0
+for filename in fileList:
+    mo = re.search(pattern, filename)
+    if mo:
+        allFileCount += 1
+        orSerList.append(mo.group(1))
+serList = orSerList.copy() # this is the list of files to be manipulated
+serList.sort() # sort the list to get ready for the insert_gap function
 
 
+# Initiate mapping of series with existing serial numbers.
+# This was done to make the code easier to follow and to be able to use
+# the previously written code.
+# If speed were an issue, this program could be written
+# without a dictionary.
+print('serList:', serList)
+serialDict = {}
+for serialNo in serList:
+    serialDict[serialNo] = serialNo
 
-if __name__ == "__main__":
-    # Confirm existence of the specified folder
-    sourceFolder = './source'
+# Find the new serial numbers
+# insList is not updated at all during the modification of serial
+# numbers because once a gap is opened, the next intersection set
+# automatically no longer includes the insertion for which the gap was
+# created.
+# The updating of the intersection set every time an insertion necessity
+# is tested and starting of modification from the smallest insertion in
+# the set guarantees gaps for all insertions.
+for serial in insList:   
+    # Find if to be inserted filenames already exist
+    intSerList = list(set(serList) & set(insList))
+    if intSerList == []: # get out of the loop as an insertion no longer needed
+        break
+    # Insert a gap in the serial numbers only for those new numbers
+    # for which there are no gaps.
+    smallest = min(intSerList)
+    position = serList.index(smallest)
+    tempList = serList[position:]
+
+    print('\nbefore tempList:', tempList)
+    tempList = insertGap(tempList)
+    print('after tempList:', tempList)
+    serList = serList[:position] + tempList
+    print('updated serList:', serList)
+
+i = 0
+for key in serialDict:
+    serialDict[key] = serList[i]
+    i += 1
+print('updated serialDict', serialDict)
+ 
+# Rename all the files after the gap
+reCount = 0 # counter for number of files renamed
+# using reverse not to destroy any files while moving them around
+for key in reversed(list(serialDict.keys())):
+    if key != serialDict[key]: # rename only if there is a change
+        reCount += 1
+        oldFilename = 'spam' + key + '.txt'
+        newFilename = 'spam' + serialDict[key] + '.txt'
+        oldFilePath = os.path.join(sourceFolder, oldFilename)
+        newFilePath = os.path.join(sourceFolder, newFilename)
+        # change file name from new to old
+        print('renaming {0} as {1}'.format(oldFilename,newFilename)) # dry run
+        shutil.move(oldFilePath, newFilePath)
+        
+print('{0} files renamed out of {1}.'.format(reCount, len(serialDict)))
     
-    ''' # for Mac OS only
-    sourceFolder = '/Users/Alper/Library/Mobile Documents/com~apple~CloudDocs' + \
-                    '/Documents/Python/Automate the boring stuff/code/insert ' + \
-                    'new files chap 9'
-    '''
-    
-    sourceFolder = os.path.abspath(sourceFolder)
-    # simpleSourceFolder = re.search(r'/([\w .-]+)$',sourceFolder).group(1)
 
-    startStr = 'spam'
-    insList = ['001', '003'] # serial numbers to be inserted
-    print('insList: ', insList)
-
-    # Confirm existence of specified directory
-    if os.path.exists(sourceFolder):
-        # Print a "welcome" message
-        print('\nWill scan files starting with ' + startStr + \
-              ' for gaps in numbering in folder:')
-        print(sourceFolder, '\n')
-    else:
-        print('Folder %s does not exist.' % (sourceFolder))
-        sys.exit()
-
-    # Get the names of the files with the specified prefix
-    fileList = os.listdir(sourceFolder)
-
-    pattern = r'spam(\d*)\.txt'
-    orSerList = [] # original file serial numbers
-
-    allFileCount = 0
-    for filename in fileList:
-        mo = re.search(pattern, filename)
-        if mo:
-            allFileCount += 1
-            orSerList.append(mo.group(1))
-    serList = orSerList.copy() # this is the list to be manipulated
-    serList.sort()
-
-
-    # Initiate mapping of series with existing serial numbers.
-    # This was done to have easier to understand code and to be able to reuse
-    # previously written code.
-    # If speed were an issue, this program could be written
-    # without a dictionary.
-    print('serList:', serList)
-    serialDict = {}
-    for serialNo in serList:
-        serialDict[serialNo] = serialNo
-
-    # Find the new serial numbers
-    # insList is not updated at all during the modification of serial numbers
-    # because once a gap is opened, the next intersection set automatically
-    # no longer includes the insertion for which the gap was created.
-    # The updating of the intersection set every time an insertion necessity is
-    # tested and starting of modification from the smallest insertion in the set
-    # guarantees gaps for all insertions
-    for serial in insList:   
-        # Find if to be inserted filenames already exist
-        intSerList = list(set(serList) & set(insList))
-        if intSerList == []: # get out of the loop as an insertion no longer needed
-            break
-        # Insert a gap in the serial numbers only for those new numbers
-        # for which there are no gaps.
-        smallest = min(intSerList)
-        position = serList.index(smallest)
-        tempList = serList[position:]
-
-        print('\nbefore tempList:', tempList)
-        tempList = insertGap(tempList)
-        print('after tempList:', tempList)
-        serList = serList[:position] + tempList
-        print('updated serList:', serList)
-
-    i = 0
-    for key in serialDict:
-        serialDict[key] = serList[i]
-        i += 1
-    print('updated serialDict', serialDict)
-     
-    # Rename all the files after the gap
-    reCount = 0 # counter for number of files renamed
-    # using reverse not to destroy any files while moving them around
-    for key in reversed(list(serialDict.keys())):
-        if key != serialDict[key]: # rename only if there is a change
-            reCount += 1
-            oldFilename = 'spam' + key + '.txt'
-            newFilename = 'spam' + serialDict[key] + '.txt'
-            oldFilePath = os.path.join(sourceFolder, oldFilename)
-            newFilePath = os.path.join(sourceFolder, newFilename)
-            # change file name from new to old
-            print('renaming {0} as {1}'.format(oldFilename,newFilename)) # dry run
-            shutil.move(oldFilePath, newFilePath)
-            
-    print('{0} files renamed out of {1}.'.format(reCount, len(serialDict)))
