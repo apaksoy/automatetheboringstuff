af0d98db256fc368720253e51fe7fc485df47ef2
apaksoy <paksoy.alper@gmail.com>
2017-11-04 12:22:38 +0300

    Update blankRowInserter.py

--- practice projects/chap 12/blankRowInserter.py
+++ practice projects/chap 12/blankRowInserter.py
@@ -48,7 +48,8 @@ for i in range(sheet.max_row, inNumber - 1, -1):
     for j in range(1, sheet.max_column + 1):
         sheet.cell(row = i + noInsert, column = j).value\
             = sheet.cell(row=i, column=j).value
-        sheet.cell(row=i, column=j).value = ''
+        sheet.cell(row=i, column=j).value = '' # this really make those
+                                               # cells empty
 
 upFilename = firstName + '_updated.' + lastName        
 wb.save(upFilename)
