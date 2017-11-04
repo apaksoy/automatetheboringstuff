95f084f7faf2f7fe6fb0acf00b3bee7da44a4a58
apaksoy <paksoy.alper@gmail.com>
2017-11-05 02:45:35 +0300

    Create textFilestoSpreadsheet.py

---
+++ practice projects/chap 12/textFilestoSpreadsheet/textFilestoSpreadsheet.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''
+Write a program to read in the contents of several text files (you can
+make the text files yourself) and insert those contents into a
+spreadsheet, with one line of text per row. The lines of the first
+text file will be in the cells of column A, the lines of the second
+text file will be in the cells of column B, and so on.
+'''
+
+import openpyxl
+
+fileNameList = ['TextOne.txt', 'TextTwo.txt', 'rj.txt']
+
+wb = openpyxl.Workbook()
+sheet = wb.active
+
+# enumerate() provides an automatic counter to be used in excel cell
+# referencing.
+for fileCounter, name in enumerate(fileNameList):
+    #some of my text files include characters with non-ASCII Unicodes 
+    txtfileObj = open(name, encoding='utf-8') 
+    lines = txtfileObj.readlines()
+    for lineCounter, line in enumerate(lines):
+        sheet.cell(row=fileCounter + 1, column=lineCounter + 1).value = line
+
+wb.save('TexttoSpreadsheet.xlsx')
+    
+    
